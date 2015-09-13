c5398ddacacd3e6b8334230864b13a6fb6bde260
Brian Ustas <brianustas@gmail.com>
2015-09-13 11:10:57 -0400

    Initial commit.

---
+++ app/js/greeting.js
@@ -0,0 +1,13 @@
+require("../css/greeting.scss")
+
+import React from "react";
+
+export default React.createClass({
+  render: function() {
+    return (
+      <div className="greeting">
+        Hello, {this.props.name}!
+      </div>
+    );
+  },
+});
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  context: __dirname + "/app",
+
+  entry: {
+    javascript: "./js/app.js",
+    html: "./index.html"
+  },
+
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: "file?name=[name].[ext]",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: 'style!css!sass'
+      }
+    ]
+  }
+};
