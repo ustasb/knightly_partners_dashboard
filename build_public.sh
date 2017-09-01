770a0b6879542ac40e69181da34d48752698e43d
Brian Ustas <brianustas@gmail.com>
2017-09-01 17:21:36 -0400

    Add comment.

--- build_public.sh
+++ build_public.sh
@@ -6,6 +6,7 @@ NODE_ENV=production ./node_modules/.bin/webpack -p --config webpack.production.c
 
 cp -r src/favicons src/index.html public
 
+# Add the Google Maps API key.
 sed_cmd='-e "s/\(maps\/api\/js\)/\1?key=$KNIGHTLY_GMAPS_API_KEY/" public/index.html'
 if [[ $(uname) == 'Darwin' ]]; then
   sed_cmd="sed -i '' $sed_cmd"
